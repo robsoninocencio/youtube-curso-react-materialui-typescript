5c4975d73bdbc472251bc97b65ec8726be9436cd
Robson Inocêncio <robson.inocencio@gmail.com>
2023-02-13 00:32:19 -0300

    feat: Criando o menu lateral (drawer)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { AppThemeProvider } from './shared/contexts/ThemeContext';
 import AppRoutes from './routes';
+import { MenuLateral } from './shared/components';
 
 export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes />
+        <MenuLateral>
+          <AppRoutes />
+        </MenuLateral>
       </BrowserRouter>
     </AppThemeProvider>
   );
---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,59 @@
+import {
+  Avatar,
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+} from '@mui/material';
+import { Box } from '@mui/system';
+
+interface IMenuLateralProps {
+  children: React.ReactNode;
+}
+
+export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
+  const theme = useTheme();
+  return (
+    <>
+      <Drawer variant="permanent">
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src="https://media.licdn.com/dms/image/C4D03AQG08tztd7CFSg/profile-displayphoto-shrink_200_200/0/1516957222041?e=1681948800&v=beta&t=Hy85Rqef-isbY4UPKSkzzujn7cvVNL8i_Tc2bt9C5rg"
+            />
+          </Box>
+          <Divider />
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
