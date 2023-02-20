3220b19e882e4ae33f74226eadd5d0782e522f97
Robson Inocêncio <robson.inocencio@gmail.com>
2023-02-20 03:22:38 -0300

    Barra de ferramentas da listagem

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,13 @@
+import { BarraDeFerramentas } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 export const Dashboard = () => {
   return (
     <LayoutBaseDePagina
       titulo="Página inicial"
-      barraDeFerramentas={<>Barra de Ferramentas</>}
+      barraDeFerramentas={
+        <BarraDeFerramentas mostrarInputBusca textoBotaoNovo="Nova" />
+      }
     >
       Testando
     </LayoutBaseDePagina>
---
+++ src/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
@@ -0,0 +1,56 @@
+import { Box, Button, Icon, Paper, TextField, useTheme } from '@mui/material';
+
+interface IBarraDeFerramentasProps {
+  textoDaBusca?: string;
+  mostrarInputBusca?: boolean;
+  aoMudarTextoDeBusca?: (novoTexto: string) => void;
+
+  textoBotaoNovo?: string;
+  mostrarBotaoNovo?: boolean;
+  aoClicarEmNovo?: () => void;
+}
+
+export const BarraDeFerramentas: React.FC<IBarraDeFerramentasProps> = ({
+  textoDaBusca = '',
+  mostrarInputBusca = false,
+  aoMudarTextoDeBusca,
+  aoClicarEmNovo,
+  textoBotaoNovo = 'Novo',
+  mostrarBotaoNovo = true,
+}) => {
+  const theme = useTheme();
+  return (
+    <Box
+      height={theme.spacing(5)}
+      marginX={1}
+      padding={1}
+      paddingX={2}
+      display="flex"
+      gap={1}
+      alignItems="center"
+      component={Paper}
+    >
+      {mostrarInputBusca && (
+        <TextField
+          size="small"
+          value={textoDaBusca}
+          placeholder="Pesquisar..."
+          onChange={(e) => aoMudarTextoDeBusca?.(e.target.value)}
+        />
+      )}
+      <Box flex={1} display="flex" justifyContent="end">
+        {mostrarBotaoNovo && (
+          <Button
+            color="primary"
+            disableElevation
+            variant="contained"
+            onClick={aoClicarEmNovo}
+            endIcon={<Icon>add</Icon>}
+          >
+            {textoBotaoNovo}
+          </Button>
+        )}
+      </Box>
+    </Box>
+  );
+};
