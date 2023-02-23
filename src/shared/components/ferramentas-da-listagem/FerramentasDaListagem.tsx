bc42c09dd7cd23a984bbd432e5e916eacca002af
Robson Inocêncio <robson.inocencio@gmail.com>
2023-02-22 22:59:09 -0300

    feat: Como configurar variáveis ambiente no React

--- src/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
+++ src/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, Icon, Paper, TextField, useTheme } from '@mui/material';
 
+import { Environment } from '../../environment';
+
 interface IFerramentasDaListagemProps {
   textoDaBusca?: string;
   mostrarInputBusca?: boolean;
@@ -34,7 +36,7 @@ export const FerramentasDaListagem: React.FC<IFerramentasDaListagemProps> = ({
         <TextField
           size="small"
           value={textoDaBusca}
-          placeholder="Pesquisar..."
+          placeholder={Environment.INPUT_DE_BUSCA}
           onChange={(e) => aoMudarTextoDeBusca?.(e.target.value)}
         />
       )}
