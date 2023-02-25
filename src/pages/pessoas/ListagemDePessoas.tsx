efbb89cb01cfdb3da11eecf8f24a7b23bfbb018b
Robson Inocêncio <robson.inocencio@gmail.com>
2023-02-25 11:55:55 -0300

    feat: Como impedir consultas desnecessárias com o useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -5,21 +5,25 @@ import { useSearchParams } from 'react-router-dom';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+import { useDebounce } from '../../shared/hooks';
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(3000, true);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    PessoasService.getAll(1, busca).then((result) => {
-      if (result instanceof Error) {
-        alert(result.message);
-      } else {
-        console.log(result);
-      }
+    debounce(() => {
+      PessoasService.getAll(1, busca).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          console.log(result);
+        }
+      });
     });
   }, [busca]);
 
