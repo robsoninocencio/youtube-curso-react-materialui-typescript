a61465cc446662d74fbce596dc0bdf7dd031afde
Robson Inocêncio <robson.inocencio@gmail.com>
2023-05-07 18:12:37 -0300

    feat: Como traduzir as mensagens de validação do Yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 
+import './shared/forms/TraducoesYup';
+
 import AppRoutes from './routes';
 import { MenuLateral } from './shared/components';
 import { DrawerProvider, AppThemeProvider } from './shared/contexts';
